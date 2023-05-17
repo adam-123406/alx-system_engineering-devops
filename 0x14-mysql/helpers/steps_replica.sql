46f126600c2010f76080c8a95d97d2bb61eb6ff1
adam-123406 <adamabdallahus@gmail.com>
2023-05-17 13:04:40 +0400

    task

--- 0x14-mysql/helpers/steps_replica.sql
+++ 0x14-mysql/helpers/steps_replica.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #Steps to establish replication process between servers
-replica_server_ip=3.227.3.18
-source_server_ip=3.235.21.36 
+replica_server_ip=54.165.92.122
+source_server_ip=54.89.107.178
 sudo ufw allow from $replica_server_ip to any port 3306
 sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
 bind-address = $source_server_ip
